f01acb9c67dc2216022def84cac117904f5ea1cb
Cyrillo46 <yahooisbetter46@gmail.com>
2024-08-04 21:33:56 -0400

    restarting project, unidentifiable bug encountered

--- src/ThemeToggle.jsx
+++ src/ThemeToggle.jsx
@@ -1,19 +1,3 @@
-import { FaSun } from "react-icons/fa";
-import { FaMoon } from "react-icons/fa";
-import { useGlobalContext } from "./Context";
-
 export const ThemeToggle = () => {
-  const { isDarkTheme, toggleDarkTheme } = useGlobalContext;
-
-  return (
-    <section className="toggle-container">
-      <button className="dark-toggle" onClick={toggleDarkTheme}>
-        {isDarkTheme ? (
-          <FaMoon className="toggle-icon" />
-        ) : (
-          <FaSun className="toggle-icon" />
-        )}
-      </button>
-    </section>
-  );
+  return <div>ThemeToggle</div>;
 };
